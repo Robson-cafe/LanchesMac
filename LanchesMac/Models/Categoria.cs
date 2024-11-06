d85c627dd6d5927bb8ead7fac648858f67e20840
Cafe <robsonveloso@hotmail.com>
2024-11-05 22:04:18 -0300

    #Commit 3 - Aula 17 - Criando model#

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        public string NomeCategoria { get; set; }
+        public string Descricao { get; set; }
+
+        public List<Lanche> Lanches { get; set; }
+
+    }
+}
