c2f5a6e8c59e34627b6baf876b30bac6c6defa9e
Cafe <robsonveloso@hotmail.com>
2024-11-06 23:08:19 -0300

    #Commit 8 - Aula 31 - Registrando repositorios - injeção de dependencia

--- LanchesMac/Program.cs
+++ LanchesMac/Program.cs
@@ -1,4 +1,6 @@
 using LanchesMac.Context;
+using LanchesMac.Repositories;
+using LanchesMac.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -7,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddTransient<ILancheRepository, LancheRepository>();
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
 
 builder.Services.AddControllersWithViews();
 
