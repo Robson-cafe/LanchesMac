be9971e48b1ccccc5fb6af1e8a79f66b5da0e15e
Cafe <robsonveloso@hotmail.com>
2024-11-26 20:42:56 -0300

    #Commit 19 - Ciadno pedidos e pedidodetalhe - aula 63

---
+++ LanchesMac/Migrations/20241126232403_PedidoDetalhes.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    /// <inheritdoc />
+    public partial class PedidoDetalhes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    PedidoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    SobreNome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Endereco1 = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Endereco2 = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Cep = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Cidede = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Telefone = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    PedidoTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TotalItensPedido = table.Column<int>(type: "int", nullable: false),
+                    PedidosEntregueEm = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.PedidoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PedidosDetalhe",
+                columns: table => new
+                {
+                    PedidoDetalheId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PedidoId = table.Column<int>(type: "int", nullable: false),
+                    LancheId = table.Column<int>(type: "int", nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PedidosDetalhe", x => x.PedidoDetalheId);
+                    table.ForeignKey(
+                        name: "FK_PedidosDetalhe_Lanches_LancheId",
+                        column: x => x.LancheId,
+                        principalTable: "Lanches",
+                        principalColumn: "LancheId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PedidosDetalhe_Pedidos_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedidos",
+                        principalColumn: "PedidoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidosDetalhe_LancheId",
+                table: "PedidosDetalhe",
+                column: "LancheId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidosDetalhe_PedidoId",
+                table: "PedidosDetalhe",
+                column: "PedidoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PedidosDetalhe");
+
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+        }
+    }
+}
---
+++ LanchesMac/Models/Pedido.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace LanchesMac.Models
+{
+    public class Pedido
+    {
+        public int PedidoId { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome")]
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe o sobrenome")]
+        [StringLength(50)]
+        public string SobreNome { get; set; }
+
+        [Required(ErrorMessage = "Informe o endereço")]
+        [StringLength(100)]
+        public string Endereco1 { get; set; }
+
+        [Required(ErrorMessage = "Complemento")]
+        [StringLength(100)]
+        public string Endereco2 { get; set; }
+
+        [Required(ErrorMessage = "Informe o CEP")]
+        [Display(Name = "CEP")]
+        [StringLength(10, MinimumLength = 8)]
+        public string Cep { get; set; }
+
+        [StringLength(20)]
+        public string Estado { get; set; }
+
+        [StringLength(50)]
+        public string Cidede { get; set; }
+
+        [Required(ErrorMessage = "Informe o telefone")]
+        [StringLength(25)]
+        [DataType(DataType.PhoneNumber)]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "Informe o e-mail")]
+        [StringLength(25)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
+                           ErrorMessage = "O email não possui formato correto")]
+        public string Email { get; set; }
+
+        [ScaffoldColumn(false)]
+        [Column(TypeName = "decimal(18,2")]
+        [Display(Name = "Total do Pedido")]
+        public decimal PedidoTotal {  get; set; }
+
+        [ScaffoldColumn(false)]
+        [Display(Name = "Itens no Pedido")]
+        public int TotalItensPedido { get; set; }
+
+        [Display(Name = "Data do Pedido")]
+        [DataType(DataType.Text)]
+        [DisplayFormat(DataFormatString = "{0 : dd/MM/yyyy hh:mm}", ApplyFormatInEditMode = true)]
+        public DataSetDateTime? PedidosEntregueEm { get; set; }
+
+        public List<PedidoDetalhe> PedidosItens {  get; set; }
+
+    }
+}
